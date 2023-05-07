ae4a28657268d4390306ea47c5423bd05a2ab815
Jelle Claes <jelleclaes0@gmail.com>
2023-05-07 14:36:25 +0200

    update spellCardSimulatorDnD5e.c

--- spellCardSimulatorDnD5e.c
+++ spellCardSimulatorDnD5e.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define MAX_LIST_SIZE (100)
 
 enum bool{false,true};
 
@@ -9,10 +10,49 @@ struct anyKey{
     char* value;
 };
 
+enum components{V,S,M};
+
+enum type{sphere,cone,cylinder,line,cube};
+
+struct area_of_effect{
+    int size;
+    enum type type;
+};
+
 struct damage_type {
     char *index;
     char *name;
     char *url;
+};
+
+enum damage_group{damage_at_character_level,damage_at_slot_level};
+
+struct damage_struct {
+    struct annyKey *keyValues;
+    struct damage_type damageType;
+};
+
+union damage {
+    struct damage_struct damage_at_character_level;
+    struct damage_struct damage_at_slot_level;
+};
+
+struct school{
+    char* index;
+    char* name;
+    char* url;
+};
+
+struct classes{
+    char* index;
+    char* name;
+    char* url;
+};
+
+struct subclasses{
+    char* index;
+    char* name;
+    char* url;
 };
 
 struct spell{
@@ -22,48 +62,31 @@ struct spell{
     char* desc;
     char* higher_level;
     char* range;
-    enum components{V,S,M};
+    enum components components[MAX_LIST_SIZE];
     char* material;
-    struct area_of_effect{
-        int size;
-        enum type{sphere,cone,cylinder,line,cube};
-    };
+    struct area_of_effect area_of_effect;
     enum bool ritual;
     char* duration;
     enum bool concentration;
     char* casting_time;
     int level;
     char* attack_type;
-    enum damage_group{damage_at_character_level,damage_at_slot_level};
-    union damage{
-        struct damage_at_character_level{
-            struct annyKey* keyValues;
-            struct damage_type damageType;
-        };
-        struct damage_at_slot_level{
-            struct annyKey* keyValues;
-            struct damage_type damageType;
-        };
-    };
-    struct school{
-        char* index;
-        char* name;
-        char* url;
-    };
-    struct classes{
-        char* index;
-        char* name;
-        char* url;
-    };
-    struct subclasses{
-        char* index;
-        char* name;
-        char* url;
-    };
+    enum damage_group damage_group;
+    union damage damage;
+    struct school school;
+    struct classes classes[MAX_LIST_SIZE];
+    struct subclasses subclasses[MAX_LIST_SIZE];
 };
 
 int main(int argc , char* argv[]){
 
-    FILE *filePointer = fopen("blur.json","r");//Open blur.json TODO NEED TO CHECK ARG TO MAIN
+    FILE *filePointer = fopen("spells/blur.json","r");//Open blur.json TODO NEED TO CHECK ARG TO MAIN
+    if (filePointer == NULL) // Check if file is succesfully opened
+    {
+        perror("File opening failed"); // Print error
+        return -1;                     // Stop program
+    }
+
+    
 
 }
